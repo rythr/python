f5ceed1230ab7bc71586200c8e27770406d1e2dd
Raki Faturahman <valeclip27@gmail.com>
2022-03-27 20:39:04 +0700

    Perulangan

---
+++ Perulangan.py
@@ -0,0 +1,11 @@
+batas_maksimal = 10
+print('Makan yang banyak gua teraktir')
+
+bakso_yang_dilahap = 0
+print(f'Bakso yang sudah dilahap {bakso_yang_dilahap}')
+
+while bakso_yang_dilahap < batas_maksimal:
+    bakso_yang_dilahap = bakso_yang_dilahap + 1
+    print(f'Bakso yang sudah dilahap {bakso_yang_dilahap} Mangkok')
+
+print(f'Bakso yang sudah habis dimakan {bakso_yang_dilahap}')
